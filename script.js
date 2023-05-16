a4cb0ae45c80ebdea2314331977c968b2ef3420d
efogerson1 <efogerson2@gmail.com>
2023-05-16 16:09:58 -0700

    Changing from multiple prompts to single prompt, and multiple confirms - will then add multiple 'if' statements

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 // Assignment code here
-const numbers = []
-const lower=[]
+// const numbers = ["1", "2", "3"];
+// const randomNumbers = Math.floor(Math.random() * numbers.length);
 
-const validOptions=[]
-const firstpicks=["numbers","lower","upper"]
-//first pick random and its index 0
-//now add a random number to retval
-//firstpicks= firstpicks.splice(0,1)
+
+// const upper = ["A", "B", "C"]
+// const randomUpper = Math.floor(Math.random() * upper.length);
+
+// const lower=["a","b","c"];
+// const randomLower = Math.floor(Math.random() * lower.length);
+
+// const validOptions=[randomNumbers, randomLower, randomUpper ]
+// const firstpicks=["numbers","lower","upper"]
+
+
+
 
 function generatePassword() {
-  /* Add prompts -- stored in variables */
-  /* Conditionals -- if they say yes to special char, select random spec char and immediatly add to returnval */
-  /* Take selected arrays, concat into returnVal and into validOptions
-  at this point we have a retval of X length and possibly want more. retval meets min user acceptance
-  loops will equal userChoice-retval.length
-  */
-  var length = 8,
+ 
+  var length = parseInt(prompt("Enter the desired length of the password:"));
+
+  if (isNaN(length) || length < 8 || length > 120) {
+    alert("Invalid length. Please enter a valid number between 8 and 120.");
+    return;
+  }
+
+  var useUppercase = confirm("Include uppercase letters?");
+  var useLowercase = confirm("Include lowercase letters?");
+  var useNumbers = confirm("Include numbers?");
+  var useSymbols = confirm("Include symbols?");
+
+  if (!useUppercase && !useLowercase && !useNumbers && !useSymbols) {
+    alert("At least one character type must be selected.");
+    return;
+  }
+
+
+ 
       charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
       retVal = "";
   for (var i = 0, n = charset.length; i < length; ++i) {
       retVal += charset.charAt(Math.floor(Math.random() * n));
+
+      
+      
+      
+      
+      
   }
   return retVal;
 }
